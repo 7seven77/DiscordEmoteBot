c2c31afd63dc5795ed3ef9e56d455097151065b4
Zain Raja <zraja1105@gmail.com>
2020-10-02 23:29:09 +0100

    Delete message when command is invoked

--- bot.py
+++ bot.py
@@ -29,6 +29,9 @@ async def hello(ctx):
 
 @bot.command(name='emote', aliases=['e'])
 async def emote(ctx, name : str):
+    # Delete the original method
+    await ctx.message.delete()
+
     path : str = f'./images/pokimane/{name}.png'
     if os.path.isfile(path):
         await ctx.send(file=discord.File(path))
