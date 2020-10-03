44e84fc2288945fafdb83fc9ecfe57e42aa7c295
Zain Raja <zraja1105@gmail.com>
2020-10-03 01:09:35 +0100

    Remove duplication when calling command
    Commands are now in the form .admiral C rather than .admiral admiralC

--- emote.py
+++ emote.py
@@ -5,15 +5,15 @@ from discord.ext import commands
 
 class emoteCog(commands.Cog):
     def __init__(self, prefix : str) -> None:
-        self.prefix = f'./images/{prefix}/'
+        self.prefix = prefix
         self._emote.name = prefix
 
     @commands.command(name='emote')
     async def _emote(self, ctx, name : str) -> None:
         # Delete the original method
-        #await ctx.message.delete()
+        await ctx.message.delete()
 
-        path : str = f'{self.prefix}{name}.png'
+        path : str = f'./images/{self.prefix}/{self.prefix}{name}.png'
         if os.path.isfile(path):
             await ctx.send(file=discord.File(path))
         else:
