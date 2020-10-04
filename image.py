04af685dc03b735a52c5e156537d89b6b4c6234f
Zain Raja <zraja1105@gmail.com>
2020-10-04 19:04:25 +0100

    Update getEmote to return Discord.File

--- image.py
+++ image.py
@@ -1,9 +1,15 @@
 from PIL import Image
+import io
+import discord
 
 mediumSize : int = (56, 56)
 largeSize : int = (112, 112)
 
-def getEmote(path : str):
+def getEmote(path : str) -> discord.File:
     image : Image = Image.open(path)
     image = image.resize(mediumSize)
-    return image
+    with io.BytesIO() as binary:
+        image.save(binary, 'PNG')
+        binary.seek(0)
+        return discord.File(binary, 'image.png')
+    
