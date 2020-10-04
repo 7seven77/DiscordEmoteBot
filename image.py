48a18b414effef803e247254c82797e38ac1872b
Zain Raja <zraja1105@gmail.com>
2020-10-04 19:09:13 +0100

    Add docstrings to image

--- image.py
+++ image.py
@@ -6,8 +6,22 @@ mediumSize : int = (56, 56)
 largeSize : int = (112, 112)
 
 def getEmote(path : str) -> discord.File:
+    """Open and resize a file ready to be sent to Discord
+
+    Parameters
+    ----------
+    path : str
+        Where the image is located
+
+    Returns
+    -------
+    discord.File
+        The image ready to be sent
+    """
+    # Open and resize the image
     image : Image = Image.open(path)
     image = image.resize(mediumSize)
+    # Convert the image to bytes then into a Discord.File
     with io.BytesIO() as binary:
         image.save(binary, 'PNG')
         binary.seek(0)
