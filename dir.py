25c473334e71c70cae90b1d1337e57c9f856d798
Zain Raja <zraja1105@gmail.com>
2020-10-05 18:39:24 +0100

    Add function to get path to image

--- dir.py
+++ dir.py
@@ -1,5 +1,7 @@
 import os
 
+from PIL.Image import NONE
+
 baseImageDirectory : str = os.path.join('.', 'images')
 
 def getImageDirectories() -> list[str]:
@@ -10,3 +12,9 @@ def getImageNames(directoryName : str) -> list[str]:
     if not os.path.isdir(path):
         return None
     return os.listdir(path)
+
+def getImagePath(imagePrefix : str, imageName : str) -> str:
+    path : str = os.path.join(baseImageDirectory, imagePrefix, f'{imagePrefix}{imageName}.png')
+    if os.path.isfile(path):
+        return path
+    return None
